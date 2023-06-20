6991f233f2a9d9f007c4b918571e1f3b993d94f1
ak22shay <ak.22akshay@gmail.com>
2023-06-19 23:07:39 -0500

    navbar commit

--- src/componants/Navbar.jsx
+++ src/componants/Navbar.jsx
@@ -1,20 +1,25 @@
 import { Button, AppBar, Typography, Badge } from "@mui/material";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import useStyles from "../styles/styles.js";
 import { ShopContext } from "../context/ShopContext.jsx";
 import { useContext } from "react";
 
 function Navbar() {
+  const navigate = useNavigate();
   const classes = useStyles();
   const { cartItems, totalCartItems } = useContext(ShopContext);
   return (
     <AppBar className={classes.appBar}>
       <div className={classes.appBarContainer}>
         <div className={classes.appBarLeftContent}>
-          <Typography variant="caption">
-            <Link to="/">Home</Link>
-          </Typography>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => navigate("/")}
+          >
+            Home
+          </Button>
         </div>
         <div className={classes.appBarRightContent}>
           <Typography variant="caption">
